83710c7121ee8759dbbb72ef1628807cad4c40c8
sheason <811627877@qq.com>
2025-02-18 11:41:16 +0800

    fix broken tauri transport

---
+++ packages/tauri/src/legacy.ts
@@ -0,0 +1,27 @@
+import { OperationType, Transport, RSPCError } from "@rspc/client";
+import { ProcedureKind } from "@rspc/client/next"; 
+import { tauriExecute } from "./next";
+
+export class TauriTransport_Legacy implements Transport {
+  async doRequest(
+    operation: OperationType,
+    key: string,
+    input: any
+  ): Promise<any> {
+    return await new Promise((resolve, reject) => {
+      const obs = tauriExecute({
+        type: operation as ProcedureKind,
+        path: key,
+        input,
+      });
+
+      obs.subscribe((v) => {
+        if (v?.code === 200) {
+          resolve(v.value);
+        } else {
+          reject(new RSPCError(v?.code ?? 0, v?.value));
+        }
+      });
+    });
+  }
+}
